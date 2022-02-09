b1ad557f2633ded02ae2214aad60be240a43b735
ssarmiento <sirsarmiento@gmail.com>
2022-02-09 11:20:48 -0400

    first commit

---
+++ ApiNet/Contexts/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ApiNet.Entities;
+using Microsoft.AspNetCore.Identity;
+using ApiNet.IdentityAuth;
+
+namespace ApiNet.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationRol> ApplicationRol { get; set; }
+
+    }
+}
